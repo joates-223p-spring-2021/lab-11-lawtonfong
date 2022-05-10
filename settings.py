524bf1d46db65398171d9e55f677285aa82d1e91
Lawton Fong <57239569+lawtonfong@users.noreply.github.com>
2022-05-09 18:18:30 -0700

    new stuff

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
